7c61fc20ed87c43bd99e62d044b214114560590e
Admin <admin@Admins-Mac.local>
2019-06-05 14:35:15 +0300

    Initial Commit

---
+++ weTravel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weTravel
+//
+//  Created by Admin on 6/5/19.
+//  Copyright © 2019 ITI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
